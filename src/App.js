e1dbcd849a15d861f303370093bbce28fb5e1f2d
AbdullahPS <abdullahtabbal@outlook.com>
2022-11-08 12:48:21 +0200

    Section 8:Index pages

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Login from './pages/Login'
 import SingleProduct from './pages/SingleProduct'
 import Home from './pages/Home'
 import Products from './pages/Products'
+import SharedLayout from './pages/SharedLayout'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="*" element={<Error />} />
