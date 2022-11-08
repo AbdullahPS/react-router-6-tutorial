1bd4bcc11513c5fc49ae45d83c8fcad04e9e0478
AbdullahPS <abdullahtabbal@outlook.com>
2022-11-08 11:55:36 +0200

    Section1:First pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Homepage</div>}></Route>
+        <Route
+          path="testing"
+          element={
+            <div>
+              <h2>Testing</h2>
+            </div>
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
