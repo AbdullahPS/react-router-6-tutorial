f4ca8d9e667f91dd4aa5b9a03ec685f9898fa7d7
AbdullahPS <abdullahtabbal@outlook.com>
2022-11-08 12:02:03 +0200

    Section 2 : Page components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Error from './pages/Error'
+import Login from './pages/Login'
+import SingleProduct from './pages/SingleProduct'
+import Home from './pages/Home'
+import Products from './pages/Products'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Homepage</div>}></Route>
-        <Route
-          path="testing"
-          element={
-            <div>
-              <h2>Testing</h2>
-            </div>
-          }
-        ></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="products" element={<Products />}></Route>
       </Routes>
     </BrowserRouter>
   )
