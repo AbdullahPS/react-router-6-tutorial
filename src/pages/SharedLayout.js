ee5710c57e7738839eac70257ec9313a6cda71c1
AbdullahPS <abdullahtabbal@outlook.com>
2022-11-08 13:12:25 +0200

    Section 9 :Navlink

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import StyledNavbar from '../components/StyledNavbar'
 const SharedLayout = () => {
   return (
     <div>
-      <Navbar />
+      <StyledNavbar />
       <Outlet />
     </div>
   )
